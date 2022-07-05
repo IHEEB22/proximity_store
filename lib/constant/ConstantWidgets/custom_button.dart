1ec862def4419eec3f2d05a73a9d65193d18033c
IHEEB22 <iheb.hmida1000@gmail.com>
2022-07-06 00:02:06 +0100

    main
    starting the custombutton widget

--- lib/constant/ConstantWidgets/custom_button.dart
+++ lib/constant/ConstantWidgets/custom_button.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:proximitystore/constant/ProxColors.dart';
 
-class custom_button extends StatelessWidget {
-  const custom_button({Key? key}) : super(key: key);
+class custom_button extends StatefulWidget {
+  final String textInput;
+  // final Color borderRadiusColor;
 
+  custom_button({
+    required this.textInput,
+    // required this.borderRadiusColor,
+  });
+  @override
+  State<custom_button> createState() => _custom_buttonState();
+}
+
+class _custom_buttonState extends State<custom_button> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 80,
+          width: 200,
+          child: OutlinedButton(
+            child: Text('howwwww',
+                // Text(toString(${this.textInput}),
+                style: TextStyle(
+                  color: Colors.white,
+                )),
+            style: OutlinedButton.styleFrom(
+              backgroundColor: ProxColors.deepblue,
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(4))),
+            ),
+            onPressed: () {
+              print('Pressed');
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:proximitystore/constant/ConstantWidgets/background_image.dart';
+import 'package:proximitystore/constant/ConstantWidgets/custom_button.dart';
 import 'package:proximitystore/constant/ProxColors.dart';
-
 import 'package:proximitystore/proxstoretest.dart';
-import 'package:proximitystore/widgets/geolocation_outside_paris.dart';
-// "create: custom backgroundImage reusable widget && geolocation page && customButton reusable widget" -m "backgroundImage widget contains image which will be used in all screens && geolocation page is a screen where the users gets redirect when he is outside of paris && customButton widget is a button which will be paramatred more to be usable for  different button accros the application (more work to do on custom_button.dart file)"void main() {
+
+void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: ProxColors.transparentColor,
     statusBarIconBrightness: ProxColors.statusbarColor,
   ));
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: const SafeArea(child: GeolocationOutsideParis()),
+    home: custom_button(
+      textInput: 'aaaa',
+    ),
   ));
 }

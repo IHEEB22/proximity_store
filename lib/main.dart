f02a1cb00b83d5353e11d69a6f8c83c69f9dfa70
IHEEB22 <iheb.hmida1000@gmail.com>
2022-07-05 22:50:28 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,7 @@ import 'package:proximitystore/constant/ProxColors.dart';
 
 import 'package:proximitystore/proxstoretest.dart';
 import 'package:proximitystore/widgets/geolocation_outside_paris.dart';
-
-void main() {
+// "create: custom backgroundImage reusable widget && geolocation page && customButton reusable widget" -m "backgroundImage widget contains image which will be used in all screens && geolocation page is a screen where the users gets redirect when he is outside of paris && customButton widget is a button which will be paramatred more to be usable for  different button accros the application (more work to do on custom_button.dart file)"void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: ProxColors.transparentColor,
     statusBarIconBrightness: ProxColors.statusbarColor,

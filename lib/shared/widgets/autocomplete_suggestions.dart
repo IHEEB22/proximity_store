9ead1d927a6f28aac391ed74eabf21e40fbed75f
IHEEB22 <iheb.hmida1000@gmail.com>
2022-07-14 16:55:02 +0100

    return SizedBox.shrink as default snapshot.connectionStatus

--- lib/shared/widgets/autocomplete_suggestions.dart
+++ lib/shared/widgets/autocomplete_suggestions.dart
@@ -11,24 +11,21 @@ class AutoCompleteSuggestions extends StatelessWidget {
     return FutureBuilder<List<Prediction>?>(
         future: context.read<LocalistaionControllerprovider>().searchLocation(context, "tun"),
         builder: (context, AsyncSnapshot<List<Prediction>?> snapshot) {
-          try {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return ListView.builder(
-                shrinkWrap: true,
-                itemCount: snapshot.data?.length,
-                itemBuilder: ((context, index) => Card(
-                      elevation: 0,
-                      child: Text(snapshot.data?[index].description ?? ''),
-                    )),
-              );
-            } else if (snapshot.connectionState == ConnectionState.waiting) {
-              CircularProgressIndicator();
-            } else {
-              return Text("error");
-            }
-          } catch (exc) {
-            print(exc);
+          if (snapshot.connectionState == ConnectionState.done) {
+            return ListView.builder(
+              shrinkWrap: true,
+              itemCount: snapshot.data?.length,
+              itemBuilder: ((context, index) => Card(
+                    elevation: 0,
+                    child: Text(snapshot.data?[index].description ?? ''),
+                  )),
+            );
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            CircularProgressIndicator();
+          } else {
+            return Text("error");
           }
+          return SizedBox.shrink();
         });
   }
 }

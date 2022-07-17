97a3111dcc9f34182f639b8cfaeea7994579e20c
Hwassef <hassine.wassef@outloook.com>
2022-07-17 11:49:43 -1100

    create app_images file to export all images and icons from assets folder nd changes some images name

---
+++ lib/constant/app_images.dart
@@ -0,0 +1,17 @@
+class AppImages {
+  AppImages._();
+
+  /// Folders
+  static const String imagesFolder = 'assets/images';
+  static const String iconsFolder = 'assets/icons';
+
+  /// Images
+  static const String backgroundImage = '$imagesFolder/background.png';
+  static const String commercePicturePlaceHolder = '$iconsFolder/commerce_picture_place_holder';
+  static const String appLogo = '$iconsFolder/app_logo.png';
+  static const String lineSheet = '$iconsFolder/line_sheet';
+  static const String pinIcon = '$iconsFolder/pin_icon.png';
+  static const String appIcon = '$iconsFolder/app_icon.png';
+  static const String sadEmoji = '$iconsFolder/sad_emoji.png';
+  static const String searchIcon = '$iconsFolder/search_icon.png';
+}
